c56b51a21cde43f7aa407d5ff136c8804d4b94c5
Abhishek shelke <abhishelke42161@gmail.com>
2026-03-16 12:13:14 +0530

    Assignment6

--- Assignment 6/db_connect.php
+++ Assignment 6/db_connect.php
@@ -1,13 +1,11 @@
 <?php
-
-
 $servername = "localhost";
 $username   = "root";
 $password   = "abhishek123";
-$database   = "ebookshop_db";
+$database   = "employee_db";
+$port       = 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
-6+
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
     die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
